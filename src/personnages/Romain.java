36807ed296d16c0da3ba8d802defb5c5caaf14e2
Tony Perottino <prt4965a@ensfsi.onmicrosoft.com>
2024-09-18 14:38:57 +0200

    v0

---
+++ src/personnages/Romain.java
@@ -0,0 +1,13 @@
+package personnages;
+
+public class Romain {
+	private String nom;
+	private int force;
+	
+	public Romain(String nom, int force) {
+		this.nom = nom;
+		this.force = force;
+	}
+	
+	
+}
